cf0d470fb303dd0d4b92171993465bb990ae92eb
Niklas Chaney <58793008+Unsynx@users.noreply.github.com>
2025-02-01 17:54:54 -0500

    Added submit button

--- frontend_hb/src/pages/WriteUpPage.jsx
+++ frontend_hb/src/pages/WriteUpPage.jsx
@@ -5,15 +5,23 @@ import axios from 'axios';
 const WriteUpPage = () => {
   const [text, setText] = useState('');
 
+  async function handleSubmit() {
+    await axios.post('http://localhost:5000/api/writeup', {
+        "text": text
+    })
+    
+  }
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
         <input
             type="input"
             value={text}
             onChange={(e) => setText(e.target.value)}
             style={{ width: '100%', padding: '0.5rem' }}
         />
-    </div>
+        <button>Submit</button>
+    </form>
   );
 };
 
